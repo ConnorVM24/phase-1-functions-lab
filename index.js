952ba3f2127189d9f492934806300547a902b219
connorvm <connormannion24@yahoo.com>
2022-10-12 16:46:57 -0400

    commit tree 6d52804c12f2d6914207e32e016182831b4d678b

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+const distance = 264;
+const Hq = 42;
+function distanceTravelledInFeet(start, finish) {
+    
+    return  Math.abs((finish - start) * distance);
+}
+
+function distanceFromHqInBlocks(blocks) {
+    return (Math.abs(blocks - Hq));
+}
+
+function distanceFromHqInFeet(blocks) {
+   const distance = distanceFromHqInBlocks(blocks);
+    return distance * 264; 
+}
